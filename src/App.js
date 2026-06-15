d368dca85fbee46ff0e918faaa712271bb3dede6
charles <charles.tyra@gmail.com>
2026-06-15 12:12:10 -0700

    adding routes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-// import logo from './favicon.svg';
+import { Routes, Route } from 'react-router';
 import './App.css';
 import PicturedDiv from './PicturedDiv';
 import background from './assets/test-image.jpg';
@@ -10,7 +10,6 @@ function App() {
         <img id='background-img' alt='background' src={background} />
       </div>
             {/* &nbsp; is used for tab */}
-      {/* &nbsp; planning on having a drop down menu of the left side. */}
       <div className='tester'>
         <PicturedDiv />
       </div>
